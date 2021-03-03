672fb2b9b3944018a8876176020307bb447182f0
ysabel-guiang <ysabelregino@yahoo.com>
2021-03-03 21:14:43 +1300

    add pseudocode to javascript

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
+//create an empty array that will store the first number given 
+//create an empty variable that will store what the user whats the calculator to do eg (multiply or divide). lets call this mode for now. eg let mode;
+//create an empty array that will store the second number given
+
+//create variable that stores the result - connect it to the result div/input text in the HTML
+
+//create a function named calculate, that will take in an argument of (value). The (value) will be dependant on what button the person presses. Eg when the 5 button is pressed, the onclick function will be called. eg calculate(5) function will be called.
+//Go back to HTML and create an onclick element for each button, then call the calculate function and put in the appropriate value
+ 
+ 
+//In the function, create if/else if statements 
+
+  //first statement - (if (value) is a number OR (value) is ".") AND mode variable is EMPTY
+    //push (value) into first number array
+
+  //second statement - (if (value) is a number OR (value) is ".") AND mode variable is NOT EMPTY
+   //push (value) into second number array
+
+   //third statement - if (value) is not a number AND value is not AC AND value is not EC AND value is not %
+    //make mode = (value)
+  
+  //third statment - if (value) is AC OR value is EC
+   //make first number, second number and mode empty
+
+   //fourth statment - if (value) is "="
+    //concat the first number array and make it into a string so it makes a whole number 
+    //store that into a variable
+    //parseInt variable to turn it into a number
+    //concat the second number array and make it into a string so it makes a whole number
+    //store that into a variable 
+    //parseInt variable to turn it into a number
+
+    //create another if/else statements 
+      //first statement - if mode is -
+        // result = number 1 - number 2 
+
+      //second statement - if mode is +
+        //result = number 1 + number 2
+
+      //third statment - if mode is /
+        //result = number 1/number 2
+
+      //fourth statement - if mode is *
+        //result = number 1 * number 2 
+
+    //clear first number, second number and mode variables
+    //return 
+    
+
+
+
 var entries = [];
 var total = 0;
 
